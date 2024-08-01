4b4383393df0c4ab304dcc1f54ca4ac94cd30735
Frank <franklaercio@gmail.com>
2024-08-01 13:31:27 -0300

    fix: try to create fix cache

--- src/main/java/br/ufrn/data/manager/infrastructure/openfeign/CacheClient.java
+++ src/main/java/br/ufrn/data/manager/infrastructure/openfeign/CacheClient.java
@@ -1,13 +1,14 @@
 package br.ufrn.data.manager.infrastructure.openfeign;
 
 import br.ufrn.data.manager.domain.OpenDataEntity;
+import br.ufrn.data.manager.infrastructure.configs.FeignConfig;
 import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(url = "${external.api.cache.url}", name = "cache-client", path = "/api/v1/cache")
+@FeignClient(url = "${external.api.cache.url}", name = "cache-client", path = "/api/v1/cache", configuration = FeignConfig.class)
 public interface CacheClient {
 
     @PostMapping
