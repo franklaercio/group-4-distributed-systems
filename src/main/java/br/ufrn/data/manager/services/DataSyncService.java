ce99773dfd9b1ad9990af6b067b671e95447d6c7
Frank <franklaercio@gmail.com>
2024-08-01 22:28:05 -0300

    fix: try to create fix cache

--- src/main/java/br/ufrn/data/manager/services/DataSyncService.java
+++ src/main/java/br/ufrn/data/manager/services/DataSyncService.java
@@ -38,9 +38,11 @@ public class DataSyncService implements ScheduleRepository {
     @Override
     public void sync(String routingKey, String datasource) {
         try {
-            OpenDataEntity openData = taxService.getRandomTax(datasource);
+            //OpenDataEntity openData = taxService.getRandomTax(datasource);
+            //logger.info("Data fetched from {} successfully: {}", datasource, openData);
+            //logger.info("Token: {}", token);
+            OpenDataEntity openData = openAccessClient.getData(datasource);
             logger.info("Data fetched from {} successfully: {}", datasource, openData);
-            logger.info("Token: {}", token);
 
             ResponseEntity<Void> cacheResponse = cacheClient.createCache(token, openData);
             if (!cacheResponse.getStatusCode().is2xxSuccessful()) {
