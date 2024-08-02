c26e6b1fb2ed6c4c6ad9c7034917981263ce3df8
Frank <franklaercio@gmail.com>
2024-08-01 23:00:42 -0300

    fix: try to create fix cache

--- src/main/java/br/ufrn/data/manager/infrastructure/openfeign/CacheClient.java
+++ src/main/java/br/ufrn/data/manager/infrastructure/openfeign/CacheClient.java
@@ -1,19 +1,16 @@
 package br.ufrn.data.manager.infrastructure.openfeign;
 
 import br.ufrn.data.manager.domain.OpenDataEntity;
-import br.ufrn.data.manager.infrastructure.configs.FeignConfig;
-import feign.Headers;
-import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@Headers("X-Application-Token: {token}")
-@FeignClient(url = "${external.api.cache.url}", name = "cache-client", path = "/api/v1/cache", configuration = FeignConfig.class)
+@FeignClient(name = "cacheClient", url = "${external.api.cache.url}")
 public interface CacheClient {
 
-    @PostMapping
-    ResponseEntity<Void> createCache(@Param("token") String token, @RequestBody OpenDataEntity openDataEntity);
+    @PostMapping("/api/v1/cache")
+    ResponseEntity<Void> createCache(@RequestHeader("X-Application-Token") String token,
+                     @RequestBody OpenDataEntity openData);
 }
